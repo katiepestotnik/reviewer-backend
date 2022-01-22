96fd3aca3bf7e04c2df1dcf2f246a05ae9c5d5b7
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-01-22 12:57:31 -0700

    add seed route

--- server.js
+++ server.js
@@ -25,11 +25,28 @@ const ReviewSchema = new mongoose.Schema({
     movieName: String,
     movieImage: String,
     movieReview: String,
+    movieRating: Number
 })
 const Review = mongoose.model("Review", ReviewSchema)
 
 app.get("/", (req, res) => {
     res.send("hello world");
+});
+app.get("/review/seed", (req, res) => {
+  // array of starter reviews
+  const startReviews = [
+    { movieName: "Pan's Labyrinth", movieImage: "https://upload.wikimedia.org/wikipedia/en/6/67/Pan%27s_Labyrinth.jpg", movieReview: "Magical", movieRating: 5 },
+    { movieName: "Blade Runner", movieImage: "https://miro.medium.com/max/1400/1*4KkBJLj0-_nGCblVAzlA2A.jpeg", movieReview: "One of my favorites.", movieRating: 5 },
+    { movieName: "The Humans", movieImage: "https://www.sho.com/site/image-bin/images/0_0_3493874/0_0_3493874_00h_1280x640.jpg", movieReview: "Utterly Boring, turned it off after 20 minutes.", movieRating: 1 }
+
+  ]
+  // Delete all reviews
+  Review.deleteMany({}, (err, data) => {
+    Review.create(startReviews,(err, data) => {
+        res.json(data);
+      }
+    );
+  });
 });
 //index
 app.get("/review", async (req, res) => {
@@ -65,6 +82,14 @@ app.post("/review", async (req, res) => {
     } catch (error) {
       res.status(400).json(error);
     }
+});
+//show
+app.get("/review/:id", async (req, res) => {
+  try {
+      res.json(await Review.findById(req.params.id));
+  } catch (error) {
+      res.status(400).json(error);
+  }
 });
   
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
